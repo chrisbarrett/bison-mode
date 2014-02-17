9f5862458206f51606f1cdc5b248e157e591bf15
Chris Barrett <chris.d.barrett@me.com>
2014-02-17 16:40:41 +1300

    Widen buffer before performing indentation

--- bison-mode.el
+++ bison-mode.el
@@ -162,61 +162,61 @@ This is the final section, after the bison grammar declarations."
 (defun bison-indent-line ()
   "Indent the current line, using C or bison formatting styles as appropriate."
   (interactive)
-  (let ((s (bison--current-line)))
+  (save-restriction
+    (widen)
+    (let ((s (bison--current-line)))
+      (save-excursion
+        (cond
+         ((or (bison--in-comment?) (bison--in-string?))
+          'noindent)
 
-    ;; Perform indentation.
-    (save-excursion
-      (cond
-       ((or (bison--in-comment?) (bison--in-string?))
-        'noindent)
+         ((bison--at-c-declarations-braces?)
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space))
 
-       ((bison--at-c-declarations-braces?)
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space))
+         ((bison--in-c-declarations-section?)
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space)
+          (indent-to bison-decl-c-column))
 
-       ((bison--in-c-declarations-section?)
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space)
-        (indent-to bison-decl-c-column))
+         ;; Indent C code blocks using C indentation. As an exception, lines
+         ;; beginning with a comment are not re-indented.
 
-       ;; Indent C code blocks using C indentation. As an exception, lines
-       ;; beginning with a comment are not re-indented.
+         ((and (bison--in-c-section?)
+               (s-matches? (rx bol "/*") (bison--current-line)) )
+          'noindent)
 
-       ((and (bison--in-c-section?)
-             (s-matches? (rx bol "/*") (bison--current-line)) )
-        'noindent)
+         ((bison--in-c-section?)
+          (c-indent-line nil t))
 
-       ((bison--in-c-section?)
-        (c-indent-line nil t))
+         ;; If multi-line, do c indent, otherwise indent as production.
+         ((bison--in-multiline-c-block?)
+          (c-indent-line nil t))
+         ((bison--in-c-block?)
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space)
+          (indent-to (+ 2 bison-production-case-column)))
 
-       ;; If multi-line, do c indent, otherwise indent as production.
-       ((bison--in-multiline-c-block?)
-        (c-indent-line nil t))
-       ((bison--in-c-block?)
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space)
-        (indent-to (+ 2 bison-production-case-column)))
+         ;; Indent productions.
 
-       ;; Indent productions.
+         ((bison--at-production-header?)
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space)
+          (indent-to 0))
 
-       ((bison--at-production-header?)
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space)
-        (indent-to 0))
+         ((bison--at-first-production-case?)
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space)
+          (indent-to (+ 2 bison-production-case-column)))
 
-       ((bison--at-first-production-case?)
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space)
-        (indent-to (+ 2 bison-production-case-column)))
+         ((or (bison--at-production-case?)
+              (bison--at-end-of-production?))
+          (goto-char (line-beginning-position))
+          (delete-horizontal-space)
+          (indent-to bison-production-case-column))))
 
-       ((or (bison--at-production-case?)
-            (bison--at-end-of-production?))
-        (goto-char (line-beginning-position))
-        (delete-horizontal-space)
-        (indent-to bison-production-case-column))))
-
-    ;; Clear dirty state if line was not actually modified.
-    (set-buffer-modified-p (not (equal s (bison--current-line))))))
+      ;; Clear dirty state if line was not actually modified.
+      (set-buffer-modified-p (not (equal s (bison--current-line)))))))
 
 ;;;; Motion Commands
 
